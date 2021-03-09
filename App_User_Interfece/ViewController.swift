83a15810c6c29ef1c129f630c19dee6ef4088506
Kozionov <e-kozionov@mail.ru>
2021-03-09 19:21:15 +1200

    Initial Commit

---
+++ App_User_Interfece/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_User_Interfece
+//
+//  Created by Evgeniy Kozionov on 09.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
